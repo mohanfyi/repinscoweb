f13975ccdc29e94aae209e0d4107db0282d3f03e
mohanfyi <mohanfyi@users.noreply.github.com>
2015-06-25 07:06:41 +0530

    Update startup-script.sh

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -16,6 +16,7 @@
 # [START startup]
 # Talk to the metadata server to get the project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/hellofastandfurious" -H "Metadata-Flavor: Google")
+PROJECTID="prjhellouniverse"
 
 # Install logging monitor and configure it to pickup application logs
 # [START logging]
