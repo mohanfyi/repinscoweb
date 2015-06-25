0f97044e268112c351db7ba446daf0a7beb22b29
mohanfyi <mohanfyi@users.noreply.github.com>
2015-06-25 18:15:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 console.log('hello world');
 var express = require('express');
+var app = express();
+
 var config = require('./config');
 require('./router/main')(app);
 
-var app = express();
+
 app.set('views',__dirname + '/views');
 app.set('view_engine','ejs');
 app.engine('html',require('ejs').renderfile);
